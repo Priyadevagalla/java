ae6d981e1ffcfdc2004d09bb02183bdcc15e06b3
Priyadevagalla <97424690+Priyadevagalla@users.noreply.github.com>
2024-07-31 20:46:08 +0530

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -15,3 +15,17 @@ public class GCD {
         System.out.println("The GCD of " + a + " and " + b + " is: " + GCD);
     }
 }
+
+
+
+
+
+/*
+Read integer a using sc.nextInt().
+Read integer b using sc.nextInt().
+Initialize the variable GCD to 1.
+For i from 1 to a:
+Check if both a % i == 0 and b % i == 0.
+If true, set GCD to i.
+Print the GCD of
+    */
